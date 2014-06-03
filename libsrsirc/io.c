375ca9d85b8be73a89d0d2862f8907c69656054a
fisted <van.fisted@googlemail.com>
2014-06-03 21:31:25 +0200

    wat

--- libsrsirc/io.c
+++ libsrsirc/io.c
@@ -53,26 +53,31 @@ io_read(sckhld sh, struct readctx *ctx, tokarr *tok, uint64_t to_us)
 
 	while (ctx->wptr < ctx->eptr && ISDELIM(*ctx->wptr))
 		ctx->wptr++; /* skip leading line delimiters */
-
 	if (ctx->wptr == ctx->eptr) /* empty buffer, use the opportunity.. */
 		ctx->wptr = ctx->eptr = ctx->workbuf;
 
+	size_t linelen;
 	char *delim;
-	while (!(delim = find_delim(ctx))) {
-		uint64_t trem = 0;
-		if (com_check_timeout(tsend, &trem))
-			return 0;
+	char *linestart;
+	do {
+		while (!(delim = find_delim(ctx))) {
+			uint64_t trem = 0;
+			if (com_check_timeout(tsend, &trem))
+				return 0;
 
-		int r = read_more(sh, ctx, trem);
-		if (r <= 0)
-			return r;
-	}
+			int r = read_more(sh, ctx, trem);
+			if (r <= 0)
+				return r;
+		}
 
-	char *linestart = ctx->wptr;
-	size_t linelen = delim - linestart;
+		linestart = ctx->wptr;
+		linelen = delim - linestart;
+		ctx->wptr++;
+	} while (linelen == 0);
+	ctx->wptr += linelen;
+
 	*delim = '\0';
 	D("read a line: '%s'", linestart);
-	ctx->wptr += linelen + 1;
 	return tokenize(linestart, tok) ? 1 : -1;
 }
 
@@ -109,7 +114,7 @@ read_more(sckhld sh, struct readctx *ctx, uint64_t to_us)
 			E("input too long");
 			return -1;
 		}
-		D("relocate");
+
 		/* make additional room by moving data to the beginning */
 		size_t datalen = (size_t)(ctx->eptr - ctx->wptr);
 		memmove(ctx->workbuf, ctx->wptr, datalen);
@@ -128,13 +133,6 @@ read_more(sckhld sh, struct readctx *ctx, uint64_t to_us)
 		n == 0 ?  W("read: EOF") : WE("read failed");
 		return -1;
 	}
-
-	printf("read (%zd): ' ", n);
-	for (ssize_t i = 0; i < n; i++)
-		printf(" %02x", ctx->eptr[i]);
-	printf("'\n");
-	
-	D("read: %zd", n);
 
 	ctx->eptr += n;
 	return 1;
