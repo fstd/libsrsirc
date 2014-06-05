73f3193ddc7c8205a0e0bebe9c969e5d35bdae8e
fisted <van.fisted@googlemail.com>
2014-06-05 16:45:14 +0200

    wat

---
+++ libsrsirc/cmap.h
@@ -0,0 +1,10 @@
+/* cmap.h - (C) 2014, Timo Buhrmester
+ * libsrsirc - a lightweight serious IRC lib - (C) 2012-14, Timo Buhrmester
+ * See README for contact-, COPYING for license information. */
+
+#ifndef LIBSRSIRC_CMAP_H
+#define LIBSRSIRC_CMAP_H 1
+
+extern const char *g_cmap[];
+
+#endif /* LIBSRSIRC_CMAP_H */
