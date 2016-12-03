408ae7fc4fa68973e99cc3af7717ec3715d1e26d
fstd <van.fstd@gmail.com>
2016-12-03 16:59:53 +0100

    horrible script for scraping a network/server list off a non-hardwired website (for testing purposes)

---
+++ scripts/mknetlist.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+#set -x
+
+site="$1"
+
+prgnam="$(basename "$0")"
+
+Nuke()
+{
+	printf '%s: ERROR: %s\n' "$prgnam" "$*" >&2
+	exit 1
+}
+
+[ -z "$site" ] && Nuke "Usage: $prgnam <site> [<throttle>]"
+
+throttle=
+[ -n "$2" ] && throttle="$2"
+
+tmp="$(mktemp /tmp/$prgnam.netlist.XXXXXXXXX)"
+netnames="$(mktemp /tmp/$prgnam.netnames.XXXXXXXXX)"
+
+trap "rm '$netnames' '$tmp'" EXIT
+trap 'exit 1' HUP INT TERM QUIT
+
+wget -qO - http://$site/networks/ | grep -o 'href="/networks/[^/]*/"' | sed -n 's,href="/networks/\([^/]*\)/",\1,p' | sort | uniq >"$netnames"
+
+c=0
+while read -r netname; do
+	wget -qO - "http://$site/servers/?net=$netname" >$tmp
+	grep -FA9001 "servers used to connect" "$tmp" | grep -FB9001 "servers last connected to" | grep -FA9001 "Main<br>Server"  \
+	    | sed 's/&#8203;//g' | pcregrep -o '<tr>.*?</tr>' | sed -e "s/ style='[^']*' *//g" -e "s/ align='[^']*' *//g" | sed 's/<td  *>/<td>/g' \
+	    | sed -e 's,</td><td>, ,g' -e 's/<tr><td>//' -e 's,</td></tr>,,' | while read -r host port ssl rest; do # oh well
+		case "$ssl" in
+			[oO][fF][fF]) sslstr= ;;
+			[oO][nN]) sslstr='/SSL' ;;
+			*) Nuke "??? (ssl is '$ssl')" ;;
+		esac
+
+		printf '%s %s:%s%s\n' "$netname" "$host" "$port" "$sslstr"
+	done
+	[ -n "$throttle" ] && sleep $throttle
+done <"$netnames"
