12c783b63831951847f262b80886ce61b99ed93c
fstd <van.fstd@gmail.com>
2016-12-03 18:21:37 +0100

    by IRC's universal rule of inconsistency, one network will actually have whitespace in its network name

--- scripts/mknetlist.sh
+++ scripts/mknetlist.sh
@@ -5,6 +5,11 @@ site="$1"
 
 prgnam="$(basename "$0")"
 
+Warn()
+{
+	printf '%s: WARNING: %s\n' "$prgnam" "$*" >&2
+}
+
 Nuke()
 {
 	printf '%s: ERROR: %s\n' "$prgnam" "$*" >&2
@@ -18,6 +23,7 @@ throttle=
 
 tmp="$(mktemp /tmp/$prgnam.netlist.XXXXXXXXX)"
 netnames="$(mktemp /tmp/$prgnam.netnames.XXXXXXXXX)"
+tab="$(printf '\t')"
 
 trap "rm '$netnames' '$tmp'" EXIT
 trap 'exit 1' HUP INT TERM QUIT
@@ -26,11 +32,13 @@ grep='pcregrep'
 command -v "$grep" >/dev/null 2>/dev/null || grep="grep -P"
 
 wget -qO - http://$site/networks/ | grep -o 'href="/networks/[^/]*/"' | sed -n 's,href="/networks/\([^/]*\)/",\1,p' | sort | uniq >"$netnames"
+cat /tmp/nl >"$netnames"
 
 c=0
 while read -r netname; do
-	wget -qO - "http://$site/servers/?net=$netname" >$tmp
-	grep -FA9001 "servers used to connect" "$tmp" | grep -FB9001 "servers last connected to" | grep -FA9001 "Main<br>Server"  \
+	chunk="$(mktemp /tmp/$prgnam.chunk.$netname.XXXXXXXXX)"
+	wget -qO - "http://$site/servers/?net=$(printf '%s\n' "$netname" | sed 's/ /%20/g')" >$chunk
+	grep -FA9001 "servers used to connect" "$chunk" | grep -FB9001 "servers last connected to" | grep -FA9001 "Main<br>Server"  \
 	    | sed 's/&#8203;//g' | $grep -o '<tr>.*?</tr>' | sed -e "s/ style='[^']*' *//g" -e "s/ align='[^']*' *//g" | sed 's/<td  *>/<td>/g' \
 	    | sed -e 's,</td><td>, ,g' -e 's/<tr><td>//' -e 's,</td></tr>,,' | while read -r host port ssl rest; do # oh well
 		case "$ssl" in
@@ -39,7 +47,15 @@ while read -r netname; do
 			*) Nuke "??? (ssl is '$ssl')" ;;
 		esac
 
-		printf '%s %s:%s%s\n' "$netname" "$host" "$port" "$sslstr"
-	done
+		printf "%s$tab%s:%s%s\\n" "$netname" "$host" "$port" "$sslstr"
+	done >$tmp
+
+	if ! [ -s "$tmp" ]; then
+		Warn "Got nothing for netname '$netname', left output in '$chunk'"
+	else
+		cat "$tmp"
+		rm "$chunk"
+	fi
+
 	[ -n "$throttle" ] && sleep $throttle
 done <"$netnames"
