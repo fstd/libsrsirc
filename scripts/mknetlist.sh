7e2cd61c675dedd5c16386d2bd2016178293a942
fstd <van.fstd@gmail.com>
2016-12-03 17:05:17 +0100

    remove an accidental BSDism

--- scripts/mknetlist.sh
+++ scripts/mknetlist.sh
@@ -22,13 +22,16 @@ netnames="$(mktemp /tmp/$prgnam.netnames.XXXXXXXXX)"
 trap "rm '$netnames' '$tmp'" EXIT
 trap 'exit 1' HUP INT TERM QUIT
 
+grep='pcregrep'
+command -v "$grep" >/dev/null 2>/dev/null || grep="grep -P"
+
 wget -qO - http://$site/networks/ | grep -o 'href="/networks/[^/]*/"' | sed -n 's,href="/networks/\([^/]*\)/",\1,p' | sort | uniq >"$netnames"
 
 c=0
 while read -r netname; do
 	wget -qO - "http://$site/servers/?net=$netname" >$tmp
 	grep -FA9001 "servers used to connect" "$tmp" | grep -FB9001 "servers last connected to" | grep -FA9001 "Main<br>Server"  \
-	    | sed 's/&#8203;//g' | pcregrep -o '<tr>.*?</tr>' | sed -e "s/ style='[^']*' *//g" -e "s/ align='[^']*' *//g" | sed 's/<td  *>/<td>/g' \
+	    | sed 's/&#8203;//g' | $grep -o '<tr>.*?</tr>' | sed -e "s/ style='[^']*' *//g" -e "s/ align='[^']*' *//g" | sed 's/<td  *>/<td>/g' \
 	    | sed -e 's,</td><td>, ,g' -e 's/<tr><td>//' -e 's,</td></tr>,,' | while read -r host port ssl rest; do # oh well
 		case "$ssl" in
 			[oO][fF][fF]) sslstr= ;;
