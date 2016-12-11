f7c28b04f626e3022b17022dce63f18e8610074a
fstd <van.fstd@gmail.com>
2016-12-11 23:33:52 +0100

    move more v3 stuff into v3 module

--- libsrsirc/v3.c
+++ libsrsirc/v3.c
@@ -455,3 +455,90 @@ handle_670(irc *ctx, tokarr *msg, size_t nargs, bool logon)
 
 	return conclude_sasl_cap(ctx) ? IO_ERR : 0;
 }
+
+size_t
+irc_v3tags_cnt(irc *ctx)
+{
+	return ctx->v3ntags;
+}
+
+static size_t
+decode_v3tag(char *dest, size_t destsz, const char *v3tag)
+{
+	size_t bc = 0;
+	bool escnext = false;
+	W("decoding");
+	while (bc < destsz) {
+		char c = *v3tag++;
+		switch (c) {
+		case '\\':// \\ -> backslash
+			if (escnext)
+				dest[bc++] = '\\';
+			escnext = !escnext;
+			continue;
+		case 's': dest[bc++] = escnext?' ' :c; break;// \s -> ' '
+		case 'r': dest[bc++] = escnext?'\r':c; break;// \r -> CR
+		case 'n': dest[bc++] = escnext?'\n':c; break;// \n -> LF
+		case ':': dest[bc++] = escnext?';' :c; break;// \: -> ; (!)
+		default:  dest[bc++] = c;
+		}
+		escnext = false;
+		if (!c)
+			break;
+	}
+
+	if (bc >= destsz) {
+		E("Tag buffer too small, result truncated");
+		dest[destsz-1] = '\0';
+	}
+
+	return bc;
+}
+
+static void
+mkv3tag(irc *ctx, size_t ind)
+{
+	decode_v3tag(ctx->v3tags_dec[ind], MAX_V3TAGLEN, ctx->v3tags_raw[ind]);
+	ctx->v3tags[ind].key = ctx->v3tags_dec[ind];
+	char *p = strchr(ctx->v3tags_dec[ind], '=');
+	if (p) {
+		*p = '\0';
+		ctx->v3tags[ind].value = p+1;
+	} else
+		ctx->v3tags[ind].value = NULL;
+}
+
+
+bool
+irc_v3tag_bykey(irc *ctx, const char *key, const char **value)
+{
+	for (size_t i = 0; i < ctx->v3ntags; i++) {
+		if (!ctx->v3tags_dec[i][0])
+			mkv3tag(ctx, i);
+
+		if (lsi_b_strcasecmp(key, ctx->v3tags[i].key) == 0) {
+			if (value)
+				*value = ctx->v3tags[i].value;
+			return true;
+		}
+	}
+	return false;
+}
+
+bool
+irc_v3tag(irc *ctx, size_t ind, const char **key, const char **value)
+{
+	if (ind >= ctx->v3ntags || !ctx->v3tags_raw[ind])
+		return false;
+
+	if (!ctx->v3tags_dec[ind][0])
+		mkv3tag(ctx, ind);
+
+	if (key)
+		*key = ctx->v3tags[ind].key;
+	if (value)
+		*value = ctx->v3tags[ind].value;
+
+	return true;
+}
+
